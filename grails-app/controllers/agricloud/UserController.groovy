2dbe43a2c2929a82bf2db28b018e01f3f2094529
Spooky <smlsun@gmail.com>
2012-10-12 11:09:41 +0800

    完成登入前後端初步串接

--- grails-app/controllers/agricloud/UserController.groovy
+++ grails-app/controllers/agricloud/UserController.groovy
@@ -4,11 +4,22 @@ class UserController {
     static scaffold = true
 
     def login = {
+    	log.info "account=${params.account}"
+    	log.info "password=${params.password}"
+
+    	//def user = User.findByAccount(params.account)
+
+    	def users = User.withCriteria {
+    		eq ('account', params.account)
+    		eq ('password', params.password)
+    	}
+
         render (contentType: 'text/json') {
             [
-                success: true,
+                success: users.size()==1,
                 session: new Date().toString().encodeAsMD5()
             ]
         }
     }
+
 }
