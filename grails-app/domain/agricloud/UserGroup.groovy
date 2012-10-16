30425079449bb35397e2b4fd2781f69f65128d80
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2012-10-16 21:55:24 +0800

    UserGroup nullable

--- grails-app/domain/agricloud/UserGroup.groovy
+++ grails-app/domain/agricloud/UserGroup.groovy
@@ -8,6 +8,6 @@ class UserGroup {
     static constraints = {
 		description nullable:true
 		//TODO 確認：有打也出現 Property [userGroupId] of class [class agricloud.UserGroup] cannot be null??
-		userGroupId nullable:true
+		//userGroupId nullable:true
     }
 }
