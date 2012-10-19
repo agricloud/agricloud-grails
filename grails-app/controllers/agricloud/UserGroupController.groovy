5741181fc153d0ff2cc73c34da5d77dea7090c76
Spooky <smlsun@gmail.com>
2012-10-19 12:55:59 +0800

    UserGroupController CRUD impl ,add runtime plugins cors

--- grails-app/controllers/agricloud/UserGroupController.groovy
+++ grails-app/controllers/agricloud/UserGroupController.groovy
@@ -1,14 +1,79 @@
 package agricloud
 
+import grails.converters.JSON
+
 class UserGroupController {
-    static scaffold = true
-	
-	def gridStore = {
-		render (contentType: 'text/json') {
-			[
-				items: UserGroup.list(),
-				totalCount: UserGroup.count()
-			]
+	static scaffold = true
+
+
+	def rest = {
+
+		switch(request.method){
+			case "POST"://save
+				def item=new UserGroup(userGroupId: request.JSON.userGroupId, description: request.JSON.description)
+				if (item.validate()) {
+					item.save();
+					response.status = 201
+					render item as JSON
+				} else {
+					sendValidationFailedResponse(item, 403)
+				}
+
+				break
+			case "GET":
+				render (contentType: 'text/json') {
+					[
+								items: UserGroup.list(),
+								totalCount: UserGroup.count()
+							]
+				}
+				break
+			case "PUT"://update
+				def item=UserGroup.findById(params.id)
+				if (!item) {
+					SendNotFoundResponse()
+				}
+				item.userGroupId = request.JSON.userGroupId
+				item.description = request.JSON.description
+
+
+				if (item.validate()) {
+					item.save();
+					response.status = 201
+					render ""
+				} else {
+					sendValidationFailedResponse(item, 403)
+				}
+				break
+			case "DELETE":
+				def item=UserGroup.findById(params.id)
+
+				if (!item) {
+					SendNotFoundResponse()
+				}
+				item.delete();
+				response.status = 204
+				render ""
+				break
+		}
+	}
+	private def sendValidationFailedResponse(item, status) {
+		response.status = status
+		render contentType: "application/xml", {
+			item {
+				item?.errors?.fieldErrors?.each {err ->
+					field(err.field)
+					message(g.message(error: err))
+				}
+			}
+		}
+	}
+	private def SendNotFoundResponse() {
+		response.status = 404
+		render contentType: "application/xml", {
+			errors {
+				message("Customer not found with id: " + params.id)
+			}
 		}
 	}
 }
